27c2e5f8c0c5e2cb768e1c14749b0329dcd97a0c
Ethan Nerney <enerney@uvm.edu>
2020-10-03 21:10:25 -0400

    Initial Commit

---
+++ tutorialGame1Tests/tutorialGame1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tutorialGame1Tests.swift
+//  tutorialGame1Tests
+//
+//  Created by Ethan Nerney on 10/3/20.
+//  Copyright © 2020 Ethan Nerney. All rights reserved.
+//
+
+import XCTest
+@testable import tutorialGame1
+
+class tutorialGame1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
